5a2b532b80bda5bcc80bf1eaaa77a1a9a67fbb84
Danilo Debiazi Vicente <dan.debiazi@gmail.com>
2020-07-24 18:45:24 -0400

    Added Translations class in the right folder and namespace

--- Domain/Translations.cs
+++ Domain/Translations.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WordsThatIKnowWebAPI.Controllers
+namespace WordsThatIKnowWebAPI.Domain
 {
     public class Translations
     {
